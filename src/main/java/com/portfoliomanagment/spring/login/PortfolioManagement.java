6ccfa592f97b00399f6a92dcefa15cb5ee6a54e2
Phyo Thu Han <phyothuhan951@gmail.com>
2024-04-27 13:32:36 +0700

    login set up

---
+++ src/main/java/com/portfoliomanagment/spring/login/PortfolioManagement.java
@@ -0,0 +1,13 @@
+package com.portfoliomanagment.spring.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagement.class, args);
+	}
+
+}
---
+++ src/main/java/com/portfoliomanagment/spring/login/models/ERole.java
@@ -0,0 +1,7 @@
+package com.portfoliomanagment.spring.login.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
